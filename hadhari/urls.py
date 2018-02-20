3a9ae4abdc4d878f7a3e88c044f16b32d77c8443
m-root <toyota100>
2018-02-20 20:42:51 +0300

    URL Edits

--- hadhari/urls.py
+++ hadhari/urls.py
@@ -1,7 +1,7 @@
-"""boxgirls URL Configuration
+"""formly URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -10,50 +10,24 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Add an import:  from blog import urls as blog_urls
+    2. Import the include() function: from django.conf.urls import url, include
+    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 
-from web import views as web
 
 urlpatterns = [
-    url(r'^', include('web.urls')),
-    url(r'^api/', include('api.urls')),
-    url(r'^calender/', include('calender.urls')),
-    url(r'^profiles/', include('core.urls', namespace='core')),
-    url(r'^reports/', include('reports.urls', namespace='reports')),
 
-    url(r'^login/$', web.LoginView.as_view(), name='login'),
-    url(r'^logout/$', web.LogoutView.as_view(), name='logout'),
-
-    url(r'^password_reset/$',
-        auth_views.password_reset,
-        {'template_name': 'web/registration/password_reset_form.html'},
-        name='password_reset'
-        ),
-    url(r'^password_reset/done/$',
-        auth_views.password_reset_done,
-        {'template_name': 'web/registration/password_reset_done.html'},
-        name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm,
-        {'template_name': 'web/registration/password_reset_confirm.html'},
-        name='password_reset_confirm'
-        ),
-    url(r'^reset/done/$',
-        auth_views.password_reset_complete,
-        {'template_name': 'web/registration/password_reset_complete.html'},
-        name='password_reset_complete'),
+    url(r"^", include('authentication.urls')),
+    url(r"^", include('core.urls', namespace='core')),
+    # url(r"^", include("map.urls"), name='map'),
 
     url(r'^admin/', admin.site.urls),
-
 ]
-
 # serve static and media files in development
 if settings.DEBUG:
     urlpatterns = urlpatterns \
