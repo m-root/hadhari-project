5e0519f528d05aac63a5b3b1262a8a545d693097
m-root <toyota100>
2018-02-20 20:44:20 +0300

    Repoorts View Updaes

--- reports/views.py
+++ reports/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from core.models import Account
 # Create your views here.
 
 def ml_reporting(request):
     # pass
     return render(request, 'reports/report.html', {})
+
+
+def anything(request):
+   x =  Account.SUPER_ADMIN
